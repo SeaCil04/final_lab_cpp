b4005d2261eedb939eae7b1a642d3459986c5c0a
SeaCil04 <92726995+SeaCil04@users.noreply.github.com>
2025-03-01 17:45:26 +0700

    Update lab24_3.cpp

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 //Write function count() here.
-
+int count(int d[],int size){
+    set<int> numset;
+    for(int i = 0;i<size;i++){
+        numset.insert(d[i]);
+    }
+    return numset.size();
+}
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
 	
